10962c1d07e645103bf58fd7cbfae71efc2eaee4
Wellington Cesar <wellingtoncgs30@gmail.com>
2019-07-16 15:35:00 -0300

    Acrescentando tracos e outras frases

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,9 +4,12 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------------------");
             Console.WriteLine("Olá Wellington, seja bem-vindo ao git!!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
+            Console.WriteLine("------------------------------------------");
         }
     }
 }
