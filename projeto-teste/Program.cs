84aa6a550803eb532542e8c3fa1e4ee773cae7ef
Wellington Cesar <wellingtoncgs30@gmail.com>
2019-07-16 15:46:37 -0300

    Corrigindo erros no projeto

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,10 +9,7 @@ namespace projeto_teste {
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
-<<<<<<< HEAD
-=======
             Console.WriteLine("------------------------------------------");
->>>>>>> 10962c1d07e645103bf58fd7cbfae71efc2eaee4
         }
     }
 }
